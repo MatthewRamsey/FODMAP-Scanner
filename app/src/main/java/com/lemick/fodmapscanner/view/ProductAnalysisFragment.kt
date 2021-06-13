e8a72c95ae1bb5c2510662fef137e964d9765ff8
Mickaël Beguin <mickael@iluv.tech>
2021-06-13 11:00:07 +0200

    Add dialog on click on an analyzed ingredient

--- app/src/main/java/com/lemick/fodmapscanner/view/ProductAnalysisFragment.kt
+++ app/src/main/java/com/lemick/fodmapscanner/view/ProductAnalysisFragment.kt
@@ -1,14 +1,18 @@
 package com.lemick.fodmapscanner.view
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.lemick.fodmapscanner.R
+import com.lemick.fodmapscanner.databinding.DialogFodmapDetailsBinding
 import com.lemick.fodmapscanner.databinding.FragmentProductAnalysisBinding
 import com.lemick.fodmapscanner.model.api.model.Product
 import com.squareup.picasso.Picasso
@@ -22,18 +26,28 @@ class ProductAnalysisFragment : Fragment() {
 
     private var _binding: FragmentProductAnalysisBinding? = null
     private val binding get() = _binding!!
+    private lateinit var dialogBinding: DialogFodmapDetailsBinding;
+    private lateinit var dialogBuilder: AlertDialog;
 
     private val productAnalysisViewModel: ProductAnalysisViewModel by inject();
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentProductAnalysisBinding.inflate(inflater, container, false)
+        dialogBinding = DialogFodmapDetailsBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initProductFromBundle()
+        initDialog()
         populateUI()
+    }
+
+    private fun initDialog() {
+        dialogBuilder = AlertDialog.Builder(this.activity).create()
+        dialogBuilder.setView(dialogBinding.root)
+        dialogBinding.dialogBtnClose.setOnClickListener { dialogBuilder.dismiss() }
     }
 
     private fun initProductFromBundle() {
@@ -44,7 +58,6 @@ class ProductAnalysisFragment : Fragment() {
         }
         val args = ProductAnalysisFragmentArgs.fromBundle(bundle)
         product = args.product
-
     }
 
     private fun populateUI() {
@@ -54,18 +67,38 @@ class ProductAnalysisFragment : Fragment() {
             val adapter = IngredientListAdapter(requireActivity(), analyzedIngredients)
             binding.productListIngredients.adapter = adapter
             binding.productListIngredients.addHeaderView(productHeader)
+            binding.productListIngredients.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                val ingredient = analyzedIngredients[position - 1]
+                ingredient.fodmapEntry?.let {
+                    dialogBinding.dialogTextIngredient.text = ingredient.ingredient.text
+                    loadFodmapLevel(it.details.fructose, dialogBinding.dialogImgFructose)
+                    loadFodmapLevel(it.details.oligos, dialogBinding.dialogImgOligos)
+                    loadFodmapLevel(it.details.lactose, dialogBinding.dialogImgLactose)
+                    loadFodmapLevel(it.details.polyols, dialogBinding.dialogImgPolyols)
+                    dialogBuilder.show()
+                }
+            }
         })
         productAnalysisViewModel.startProductAnalysis(product)
         val textProductName = productHeader.findViewById<TextView>(R.id.text_product_name)
         textProductName.text = product.productName
-        if (product.imageFrontSmallUrl != null) {
+        product.imageFrontSmallUrl?.let {
             val imageProduct = productHeader.findViewById<ImageView>(R.id.image_product)
             Picasso.get()
-                .load(product.imageFrontUrl)
+                .load(it)
                 .fit().centerInside()
-                .transform(RoundedCornersTransformation(5,5))
+                .transform(RoundedCornersTransformation(5, 5))
                 .placeholder(R.drawable.progress_animation)
                 .into(imageProduct)
+        }
+    }
+
+    fun loadFodmapLevel(level: Int, imageView: ImageView) {
+        Picasso.get().load(R.mipmap.ic_valid).into(imageView)
+        when {
+            level == 0 -> Picasso.get().load(R.mipmap.ic_valid).into(imageView)
+            level == 1 -> Picasso.get().load(R.mipmap.ic_warning).into(imageView)
+            level == 2 -> Picasso.get().load(R.mipmap.ic_alert).into(imageView)
         }
     }
 
